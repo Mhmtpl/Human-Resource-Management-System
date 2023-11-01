da165f9800976c7f28a5d4e8003cfc7c722935ae
Pala <Pala@DESKTOP-C50L9C8>
2023-11-01 13:52:03 +0300

    Add project files.

---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+
+    public class Employee : BaseEntity
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int EmployeeId { get; set; }
+    }
+}
---
+++ Human-Resource-Management-System/RepositoryContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Human_Resource_Management_System
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+
+}
